321b5ea2ec1829f493d49a29ecd74eca4039ca44
chpstck <nicolas.enz@outlook.com>
2022-03-04 10:45:28 -0300

    Initial commit

---
+++ Contador/Main.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Contador
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.count = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Contador";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.count.Location = new System.Drawing.Point(143, 52);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(72, 86);
+            this.count.TabIndex = 0;
+            this.count.Text = "0";
+            this.count.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 210);
+            this.Controls.Add(this.count);
+            this.Name = "Main";
+            this.Text = "Contador";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label count;
+    }
+}
+
---
+++ Contador/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Contador
+{
+    public partial class Main : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int HOTKEY_ID = 1;
+        
+        public Main()
+        {
+            RegisterHotKey(this.Handle, HOTKEY_ID, 2, (int)Keys.D9);
+            InitializeComponent();
+        }
+        
+        
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                int contador = int.Parse(count.Text);
+                contador++;
+                count.Text = contador.ToString();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            if (FormWindowState.Minimized == WindowState)
+                WindowState = FormWindowState.Normal;
+            Activate();
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 1);
+        }
+    }
+}
